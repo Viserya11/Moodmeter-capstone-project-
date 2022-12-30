ab7ffbb074ff93150bc115444028d0720b699934
Panna Szűcs <szucspann@gmail.com>
2022-12-30 17:22:52 +0100

    displaying questions, formatting

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,61 +1,90 @@
-import React from 'react'
-import { Button, Container } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { Button, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default function Test() {
+  const [emotions, setEmotions] = useState([]);
+  const [answers, setAnswers] = useState({});
+
+  useEffect(() => {
+    getQuestions();
+  }, []);
+
+  async function getQuestions() {
+    try {
+      const response = await fetch(
+        "https://raw.githubusercontent.com/Viserya11/capstone-project/master/emotions.json"
+      );
+      if (response.ok) {
+        let emotions = await response.json();
+        setEmotions(emotions.emotions);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleChange = (event, emotion, example) => {
+    setAnswers({
+      ...answers,
+      [emotion]: {
+        ...answers[emotion],
+        [example]: event.target.value,
+      },
+    });
+  };
+
   return (
     <>
-    <h2>Take test</h2>
-    <Container className='testcontainer'>
-    
-        <p>Question about something something</p>
-        <div className="container d-flex">
-        <label>Agree
-         
-      <input className='checkmark' type="checkbox" checked="checked"/>
-        <span class="checkmark"></span>
-      </label>
+      <h2>Take test</h2>
+      <Container className="testcontainer">
+        {emotions.map((emotion) => (
+          <div key={emotion.name}>
+            <ul>
+              {emotion.examples.map((example) => (
+                <li key={example}>
+                  <h6> {example}</h6>
 
-      <label>Disagree
-      <input className='checkmark' type="checkbox"/>
-      <span class="checkmark"></span>
-     </label>
-        </div>
+                  <div className="container d-flex">
+                    <label>
+                      Agree
+                      <input
+                        type="radio"
+                        value="agree"
+                        checked={
+                          answers[emotion] &&
+                          answers[emotion][example] === "agree"
+                        }
+                        onChange={(event) =>
+                          handleChange(event, emotion, example)
+                        }
+                      />
+                    </label>
+                    <label>
+                      Disagree
+                      <input
+                        type="radio"
+                        value="disagree"
+                        checked={
+                          answers[emotion] &&
+                          answers[emotion][example] === "disagree"
+                        }
+                        onChange={(event) =>
+                          handleChange(event, emotion, example)
+                        }
+                      />
+                    </label>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </Container>
 
-        <p>Question about something something</p>
-        <div className="container d-flex">
-        <label>Agree
-         
-      <input className='checkmark' type="checkbox" checked="checked"/>
-        <span class="checkmark"></span>
-      </label>
-
-      <label>Disagree
-      <input className='checkmark' type="checkbox"/>
-      <span class="checkmark"></span>
-     </label>
-        </div>
-
-        <p>Question about something something</p>
-        <div className="container d-flex">
-        <label>Agree
-         
-      <input className='checkmark' type="checkbox" checked="checked"/>
-        <span class="checkmark"></span>
-      </label>
-
-      <label>Disagree
-      <input className='checkmark' type="checkbox"/>
-      <span class="checkmark"></span>
-     </label>
-        </div>
-
-       
-        
-    </Container>
-    
-    <Link to="/profile"><Button className='submitbtn'>Submit</Button></Link>
-    
+      <Link to="/profile">
+        <Button className="submitbtn">Submit</Button>
+      </Link>
     </>
-  )
+  );
 }
