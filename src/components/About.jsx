c51b1aa85816d5c92520d8ecb7bcf2f4e22bf6fe
Panna Szűcs <szucspann@gmail.com>
2023-01-18 09:12:12 +0100

    hardcoded content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,33 @@
-import React from 'react'
-import { Button } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default function About() {
   return (
     <>
-    <h2>About</h2>
-    <div className='aboutcontainer'>
-      <div className='d-flex'><span><img src="" alt="" /></span><span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia distinctio optio cumque rerum praesentium voluptas. Ullam hic sequi, cum assumenda tempore numquam tenetur ab, nisi commodi dolorem incidunt! Quidem, eligendi!</span></div>
-    </div>
-   <Link to="/test"><Button>Take test</Button></Link> 
+      <h2>About</h2>
+      <div className="aboutcontainer">
+        <div className="d-flex">
+          <span>
+            
+            <img
+              className="bigfive"
+              src="https://raw.githubusercontent.com/Viserya11/capstone-project/master/src/big_five.png"
+              alt=""
+            />
+          </span>
+          <span className="justify">
+          <h5>Big 5</h5>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia
+            distinctio optio cumque rerum praesentium voluptas. Ullam hic sequi,
+            cum assumenda tempore numquam tenetur ab, nisi commodi dolorem
+            incidunt! Quidem, eligendi!
+          </span>
+        </div>
+      </div>
+      <Link to="/test">
+        <Button>Take test</Button>
+      </Link>
     </>
-  )
+  );
 }
