350e216c8f0b2c8a2f52379f0bc3cd00a0b9d470
Panna Szűcs <szucspann@gmail.com>
2023-01-18 09:07:58 +0100

    css

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,11 @@ import { Link } from 'react-router-dom'
 
 export default function About() {
   return (
-    <><div>About</div>
+    <>
+    <h2>About</h2>
+    <div className='aboutcontainer'>
+      <div className='d-flex'><span><img src="" alt="" /></span><span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia distinctio optio cumque rerum praesentium voluptas. Ullam hic sequi, cum assumenda tempore numquam tenetur ab, nisi commodi dolorem incidunt! Quidem, eligendi!</span></div>
+    </div>
    <Link to="/test"><Button>Take test</Button></Link> 
     </>
   )
--- src/components/Jumbo.jsx
+++ src/components/Jumbo.jsx
@@ -7,11 +7,13 @@ export default function Jumbo() {
   return (
     <>
         <Jumbotron>
-        <h1>MoodMeter</h1>  
+        
         <div className='jumboflex'>
+          <div><h1>MoodMeter</h1>  
          <p className='justify'>
       An application to monitor your current mood and get your Big 5 test results the same time.
             </p>
+            </div>
             <p><img className='logo' src="https://raw.githubusercontent.com/Viserya11/capstone-project/master/src/psychiatry.png" alt="logo" /></p>
             </div>
             
